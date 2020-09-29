7c87ba1cdbd3e35c342ac6750d9b04f74759de6f
jmarshall5 <jmarshall5@chewy.com>
2020-09-29 09:59:47 -0400

    Initial Commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This doesn't actually do anything, it's just hear to complile a pringBoot app
+ * that the 'bmuschko' plugin can work with.
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
